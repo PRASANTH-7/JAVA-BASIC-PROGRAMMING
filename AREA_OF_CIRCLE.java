35bcc2146c3a39885bbfa428731f3a17fa16ae84
BHASKARAPRASATH SK <116350913+PRASANTH-7@users.noreply.github.com>
2023-08-18 18:29:18 +0530

    Update AREA_OF_CIRCLE.java

--- AREA_OF_CIRCLE.java
+++ AREA_OF_CIRCLE.java
@@ -1,21 +1,12 @@
 /* JAVA PROGRAM TO FIND AREA OF CIRCLE */
-
 import java.util.*;
 public class AREA_OF_CIRCLE {
-
   public static void main(String[]args){
-  
   Scanner sc=new Scanner(System.in);
-  
   int number=sc.nextInt();
-  
-  float pi=3.14f;
-  
-  int radius=number*number;
-  
-  float area=pi*(float)radius;
-  
-  System.out.println(area);
-  
+  float pi=3.14f; 
+  int radius=number*number;  
+  float area=pi*(float)radius;  
+  System.out.println(area); 
   }
 }
