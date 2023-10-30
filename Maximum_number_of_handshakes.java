a37c3419faa51ad7955c7f0bb68e6f6f7cc9043b
BHASKARAPRASATH SK <116350913+PRASANTH-7@users.noreply.github.com>
2023-10-30 14:04:25 +0530

    Create Maximum_number_of_handshakes.java

---
+++ Maximum_number_of_handshakes.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Maximum_number_of_handshakes {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+    
+      System.out.println(n*(n-1)/2);
+      
+  }
+}
